0ae2a6f3af8a6e2f731ad80996b0906490977852
LAPTOP-T83U7O5V\Ulysse <bhbj>
2022-01-06 18:13:22 +0100

    jcplus

--- Hyzahaque/Assets/Scripts/Map/LittleFly.cs
+++ Hyzahaque/Assets/Scripts/Map/LittleFly.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class LittleFly : MonoBehaviour
 {
+    public SpriteRenderer sprite;
     public Animator flyAnim;
     // Start is called before the first frame update
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
         //flyAnim = GetComponent<Animator>;
     }
 
@@ -16,5 +18,9 @@ public class LittleFly : MonoBehaviour
     {
         float speed = Random.Range(0.5f, 1.5f);
         flyAnim.speed = speed;
+    }
+    public void RandomizeColor()
+    {
+        sprite.color = new Color(1, 1, 1, Random.Range(0.5f, 1));
     }
 }
