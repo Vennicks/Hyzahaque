10058dfc526e6cbe22d3be04a8d977f37bbc2381
WyZZmpr <maxime.pitou@ynov.com>
2022-01-07 10:17:16 +0100

    Adding UI

---
+++ Hyzahaque/Assets/Scripts/UI/LifeBar/HealtsHeartVisual.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealtsHeartVisual : MonoBehaviour
+{
+    private Image CreateHeartImage()
+    {
+        GameObject heartGameObject = new GameObject("Heart",typeof(Image));
+        return null;
+    }
+}
